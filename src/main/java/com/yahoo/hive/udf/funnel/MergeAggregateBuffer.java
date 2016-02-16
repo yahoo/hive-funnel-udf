556a5124c32e648ae183495a9405bdb55817a4b3
Josh Walters <josh@joshwalters.com>
2016-02-16 14:51:28 -0800

    Working on some refactors

--- src/main/java/com/yahoo/hive/udf/funnel/MergeAggregateBuffer.java
+++ src/main/java/com/yahoo/hive/udf/funnel/MergeAggregateBuffer.java
@@ -18,8 +18,53 @@ package com.yahoo.hive.udf.funnel;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.hadoop.hive.ql.exec.UDFArgumentTypeException;
+import org.apache.hadoop.hive.ql.metadata.HiveException;
 import org.apache.hadoop.hive.ql.udf.generic.GenericUDAFEvaluator.AggregationBuffer;
 
+/**
+ * Merges funnels into an aggregate.
+ */
 class MergeAggregateBuffer implements AggregationBuffer {
+    /** Stores funnel aggregate. */
     List<Long> elements = new ArrayList<>();
+
+    /**
+     * Add a funnel to the aggregate.
+     *
+     * @param funnel Funnel in the form of a list of longs.
+     */
+    public void addFunnel(List<Long> funnel) throws HiveException {
+        // If empty, just copy elements
+        if (elements.isEmpty()) {
+            elements.addAll(funnel);
+        } else {
+            // If the sizes don't match, throw an exception
+            if (elements.size() != funnel.size()) {
+                throw new UDFArgumentTypeException(0, "Funnels must be of the same size to merge!");
+            }
+            // Not empty, merge with existing list
+            for (int i = 0; i < funnel.size(); i++) {
+                elements.set(i, (funnel.get(i) + elements.get(i)));
+            }
+        }
+    }
+
+    /**
+     * Clear the aggregate.
+     */
+    public void clear() {
+        elements.clear();
+    }
+
+    /**
+     * Output aggregate. Must do a deep copy, and return a new list.
+     *
+     * @return Funnel aggregate counts.
+     */
+    public List<Long> output() {
+        List<Long> values = new ArrayList<Long>(elements.size());
+        values.addAll(elements);
+        return values;
+    }
 }
